dc281a22a7d37774be0ce5d4d843f94671d0bc7d
MarcinMichnik <marcin.michnik@o2.pl>
2021-07-08 19:22:33 +0200

    :sparkles: scaffolded fruit controller and json serialization

---
+++ VegeMarketProject/Models/Fruit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VegeMarketProject
+{
+    public class Fruit : Item
+    {
+        public Fruit(DateTime? expirationDate, Double quality, Double weight)
+        {
+            _added = DateTime.Now;
+            _expirationDate = expirationDate;
+            // These fields cannot be less than 0
+            _quality = quality < 0 ? 0 : quality;
+            _weight = weight < 0 ? 0 : weight;
+        }
+    }
+}
---
+++ VegeMarketProject/Models/FruitContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace VegeMarketProject.Models
+{
+    public class FruitContext : DbContext
+    {
+        public FruitContext(DbContextOptions<FruitContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Fruit> Fruits { get; set; }
+    }
+}
