31474ec9f86bb0776a54be881f8810ea8cd99196
MarcinMichnik <marcin.michnik@o2.pl>
2021-07-09 19:36:39 +0200

    :sparkles: runasync

--- VegeMarketClient/Program.cs
+++ VegeMarketClient/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Client;
 using SecureAPIClient;
 
 namespace VegeMarketClient
@@ -28,5 +29,35 @@ namespace VegeMarketClient
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static async Task RunAsync()
+        {
+            AuthConfig config = AuthConfig.ReadFromJsonFile("appsettings.json");
+
+            IConfidentialClientApplication app;
+
+            app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                .WithClientSecret(config.ClientSecret)
+                .WithAuthority(new Uri(config.Authority))
+                .Build();
+
+            string[] ResourceIds = new string[] { config.ResourceID };
+
+            AuthenticationResult result = null;
+            try
+            {
+                result = await app.AcquireTokenForClient(ResourceIds).ExecuteAsync();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Token acquired \n");
+                Console.WriteLine(result.AccessToken);
+                Console.ResetColor();
+            }
+            catch (MsalClientException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+        }
     }
 }
